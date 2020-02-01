aad1e28aa7c093e2fe655a675cf76c0d3135e618
Zhangyuan Nie <yuan@zynie.dev>
2020-02-01 14:05:33 -0500

    backend: basic wss

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,23 @@
+const WebSocket = require('ws')
 const mongoose = require('mongoose')
 
-mongoose.connect('insert address', { useNewUrlParser: true })
+const config = require('./config.json')
+
+mongoose.connect(config.mongoString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 mongoose.set('useCreateIndex', true)
+
+const wss = new WebSocket.Server({ port: config.port })
+
+wss
+  .on('connection', ws => {
+    ws.on('message', msg => {
+      console.log('received: %s', msg)
+    })
+    ws.send('Message received!')
+  })
+  .on('listening', () => {
+    console.log(`Listening on ${config.port}...`)
+  })
